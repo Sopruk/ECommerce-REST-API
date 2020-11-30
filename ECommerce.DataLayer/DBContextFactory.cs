4321e10eb3f1f9cd8774d411e9002c1e70829f93
Sopruk <alexandr.programer@gmail.com>
2020-11-30 22:27:19 +0100

    added Context to DL

---
+++ ECommerce.DataLayer/DBContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerce.DataLayer
+{
+    public class DBContextFactory : IDesignTimeDbContextFactory<EFDBContext>
+
+    {
+        public EFDBContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<EFDBContext>();
+            optionsBuilder.UseSqlServer("Data Source=ARM1;Integrated Security=True;Database=Identety;Trusted_Connection=True;MultipleActiveResultSets=true", b => b.MigrationsAssembly("ECommerce.DataLayer"));
+            return new EFDBContext(optionsBuilder.Options);
+        }
+    }
+}
---
+++ ECommerce.DataLayer/EFDBContext.cs
@@ -0,0 +1,13 @@
+﻿using ECommerce.DataLayer.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.DataLayer
+{
+    public class EFDBContext: DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public EFDBContext(DbContextOptions options):base(options)
+        {
+        }
+    }
+}
